8fa3dbbd92d33400b6fb5fb6dae47ea566617ec3
Joe Liao <joe3727@gmail.com>
2024-12-14 14:32:21 +0800

    新增 Google Analytics 事件追蹤以記錄 404 錯誤頁面的瀏覽

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -49,6 +49,10 @@ const fetchRandomQuote = () => {
 onMounted(() => {
   fetchRandomMeme()
   fetchRandomQuote()
+  gtag('event', 'page_view', {
+    event_category: 'Error',
+    event_label: '404 Page'
+  })
 })
 </script>
 

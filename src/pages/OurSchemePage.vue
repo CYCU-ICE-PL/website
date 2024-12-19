6eeb9ff6636f78e7169862034310f15e083e99a3
Joe Liao <joe3727@gmail.com>
2024-12-19 23:29:38 +0800

    更新教學頁面，將單一圖片改為多圖片顯示，調整步驟標題及描述，新增步驟圖片

--- src/pages/OurSchemePage.vue
+++ src/pages/OurSchemePage.vue
@@ -1,174 +1,189 @@
 <template>
-    <WebSocketComponent v-slot="{ isConnected: wsConnected, sendMessage, connect, disconnect, connecting }"
-      @message="updateOutput" @connected="handleConnected" @disconnected="handleDisconnected">
-      <q-page class="q-pa-sm">
-        <div class="row q-col-gutter-md" style="text-align: center">
+  <WebSocketComponent v-slot="{ isConnected: wsConnected, sendMessage, connect, disconnect, connecting }"
+    @message="updateOutput" @connected="handleConnected" @disconnected="handleDisconnected">
+    <q-page class="q-pa-sm">
+      <div class="row q-col-gutter-md" style="text-align: center">
+        <div class="col-12">
+          <q-btn-group push>
+            <q-select filled v-model="interpreterType" :options="interpreterOptions" label="選擇"
+              :disable="isInterpreterTypeLocked">
+              <q-tooltip anchor="bottom right" self="top middle"> 選擇欲測試project編號 </q-tooltip>
+            </q-select>
+            <template v-if="wsConnected">
+              <q-btn icon="link_off" @click="disconnect" color="red" round>
+                <q-tooltip anchor="bottom middle" self="top middle"> 中斷連線 </q-tooltip>
+              </q-btn>
+            </template>
+            <template v-else>
+              <q-btn icon="link" @click="() => connect(`OurScheme${interpreterType}`)" color="primary" round v-if="!connecting">
+                <q-tooltip anchor="bottom middle" self="top middle"> 點擊連線 </q-tooltip>
+              </q-btn>
+              <q-spinner-tail v-else color="grey" class="q-ma-sm" size="md" />
+            </template>
+          </q-btn-group>
           <div class="col-12">
-            <q-btn-group push>
-              <q-select filled v-model="interpreterType" :options="interpreterOptions" label="選擇"
-                :disable="isInterpreterTypeLocked">
-                <q-tooltip anchor="bottom right" self="top middle"> 選擇欲測試project編號 </q-tooltip>
-              </q-select>
-              <template v-if="wsConnected">
-                <q-btn icon="link_off" @click="disconnect" color="red" round>
-                  <q-tooltip anchor="bottom middle" self="top middle"> 中斷連線 </q-tooltip>
-                </q-btn>
-              </template>
-              <template v-else>
-                <q-btn icon="link" @click="() => connect(`OurScheme${interpreterType}`)" color="primary" round v-if="!connecting">
-                  <q-tooltip anchor="bottom middle" self="top middle"> 點擊連線 </q-tooltip>
-                </q-btn>
-                <q-spinner-tail v-else color="grey" class="q-ma-sm" size="md" />
-              </template>
-            </q-btn-group>
-            <div class="col-12">
-              <q-input filled v-model="code" label="輸入程式碼" type="textarea" autogrow class="q-mt-md" :spellcheck="false"
-                :disable="!wsConnected || executing">
-                <template v-slot:before>
-                  <template v-if="!executing">
-                    <q-btn v-if="wsConnected" icon="play_arrow" @click="executeCode(sendMessage)" color="green" round
-                      size="md">
-                      <q-tooltip anchor="bottom middle" self="top middle"> 執行程式碼 </q-tooltip>
-                    </q-btn>
-                    <q-btn v-if="wsConnected" icon="visibility" @click="sendPostRequest" color="blue" round size="md">
-                      <q-tooltip anchor="bottom middle" self="top middle"> 可視化分析 </q-tooltip>
-                    </q-btn>
-                  </template>
-                  <q-spinner v-else color="grey" size="xs" />
+            <q-input filled v-model="code" label="輸入程式碼" type="textarea" autogrow class="q-mt-md" :spellcheck="false"
+              :disable="!wsConnected || executing">
+              <template v-slot:before>
+                <template v-if="!executing">
+                  <q-btn v-if="wsConnected" icon="play_arrow" @click="executeCode(sendMessage)" color="green" round
+                    size="md">
+                    <q-tooltip anchor="bottom middle" self="top middle"> 執行程式碼 </q-tooltip>
+                  </q-btn>
                 </template>
-              </q-input>
-            </div>
-          </div>
-          <div class="col-6">
-            <TextArea :initialText="input" :title="inputTitle" @update:text="updateInput" />
-          </div>
-          <div class="col-6">
-            <TextArea :initialText="output" :title="outputTitle" readonly />
+                <q-spinner v-else color="grey" size="xs" />
+              </template>
+              <template v-slot:append>
+                <q-btn
+                  flat
+                  round
+                  :icon="generateTree ? 'visibility' : 'visibility_off'"
+                  :color="generateTree ? 'green' : 'grey'"
+                  @click="generateTree = !generateTree"
+                  size="sm"
+                >
+                  <q-tooltip anchor="bottom middle" self="top middle">
+                    {{ generateTree ? '已開啟可視化分析' : '已關閉可視化分析' }}
+                  </q-tooltip>
+                </q-btn>
+              </template>
+            </q-input>
           </div>
         </div>
-      </q-page>
-      <q-dialog v-model="dialogVisible" full-height full-width>
-        <q-card flat>
-          <ParseTree :parseTree="parseTree" />
-          <q-card-actions align="right">
-            <q-btn label="關閉" color="primary" v-close-popup />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
-    </WebSocketComponent>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import { useQuasar } from 'quasar'
-  import TextArea from 'components/TextArea.vue'
-  import WebSocketComponent from 'components/WebsocketComponent.vue'
-  import ParseTree from 'components/ParseTree.vue'
-  import axios from 'axios'
-  
-  const $q = useQuasar()
-  
-  const code = ref('')
-  const output = ref('')
-  const input = ref('')
-  const inputTitle = ref('Input')
-  const outputTitle = ref('Output')
-  const interpreterType = ref('project1')
-  const isInterpreterTypeLocked = ref(false)
-  const executing = ref(false)
-  const dialogVisible = ref(false)
-  const parseTree = ref('')
-  
-  const interpreterOptions = ['project1', 'project2, 3, 4']
-  
-  const executeCode = (sendMessage) => {
-    executing.value = true
-    code.value += '\n' // 添加換行符
-    input.value += code.value
-    const message = {
-      interpreterType: `OurScheme${interpreterType.value}`,
-      payload: code.value,
+        <div class="col-6">
+          <TextArea :initialText="input" :title="inputTitle" @update:text="updateInput" />
+        </div>
+        <div class="col-6">
+          <TextArea :initialText="output" :title="outputTitle" readonly />
+        </div>
+      </div>
+    </q-page>
+    <q-dialog v-model="dialogVisible" full-height full-width>
+      <q-card flat>
+        <ParseTree :parseTree="parseTree" />
+        <q-card-actions align="right">
+          <q-btn label="關閉" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </WebSocketComponent>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useQuasar } from 'quasar'
+import TextArea from 'components/TextArea.vue'
+import WebSocketComponent from 'components/WebsocketComponent.vue'
+import ParseTree from 'components/ParseTree.vue'
+import axios from 'axios'
+
+const $q = useQuasar()
+
+const code = ref('')
+const output = ref('')
+const input = ref('')
+const inputTitle = ref('Input')
+const outputTitle = ref('Output')
+const interpreterType = ref('project1')
+const isInterpreterTypeLocked = ref(false)
+const executing = ref(false)
+const dialogVisible = ref(false)
+const parseTree = ref('')
+const generateTree = ref(false)
+
+const interpreterOptions = ['project1', 'project2, 3, 4']
+
+const executeCode = async (sendMessage) => {
+  executing.value = true
+  code.value += '\n' // 添加換行符
+  input.value += code.value
+  const message = {
+    interpreterType: `OurScheme${interpreterType.value}`,
+    payload: code.value,
+  }
+  sendMessage(JSON.stringify(message))
+  gtag('event', 'execute_code', {
+    event_category: 'Code Execution',
+    event_label: `OurScheme${interpreterType.value}`,
+  })
+  if (generateTree.value) {
+    await sendPostRequest()
+  }
+  code.value = '' // 清空輸入程式碼
+  setTimeout(() => {
+    executing.value = false
+  }, 1000) // 模擬執行完成後的狀態變更
+}
+
+const updateInput = (newInput) => {
+  input.value = newInput
+}
+
+const updateOutput = (message) => {
+  output.value += message // 將 WebSocket 回傳的訊息添加到 output
+}
+
+const lockInterpreterType = () => {
+  isInterpreterTypeLocked.value = true
+}
+
+const unlockInterpreterType = () => {
+  isInterpreterTypeLocked.value = false
+}
+
+const clearInputOutput = () => {
+  input.value = ''
+  output.value = ''
+}
+
+const handleConnected = () => {
+  lockInterpreterType()
+  clearInputOutput()
+  $q.notify({
+    type: 'positive',
+    message: '連線成功',
+    timeout: 1200,
+    position: 'top',
+    progress: true,
+  })
+}
+
+const handleDisconnected = () => {
+  unlockInterpreterType()
+  $q.notify({
+    type: 'warning',
+    message: '連線中斷',
+    timeout: 1200,
+    position: 'top',
+    progress: true,
+    icon: 'warning'
+  })
+}
+
+const sendPostRequest = async () => {
+  try {
+    const response = await axios.post(`https://visualpl.lab214b.uk:5001/syntax-tree`, {
+      payload: code.value + '\n',
+      interpreterType: `OurScheme${interpreterType.value}`
+    })
+    if (response.status !== 200) {
+      throw new Error('請檢查輸入是否正確')
     }
-    sendMessage(JSON.stringify(message))
-    code.value = '' // 清空輸入程式碼
-    gtag('event', 'execute_code', {
-      event_category: 'Code Execution',
+    parseTree.value = response.data.parseTree
+    dialogVisible.value = true
+    gtag('event', 'visualize_syntax_tree', {
+      event_category: 'Syntax Tree',
       event_label: `OurScheme${interpreterType.value}`,
     })
-    setTimeout(() => {
-      executing.value = false
-    }, 1000) // 模擬執行完成後的狀態變更
-  }
-  
-  const updateInput = (newInput) => {
-    input.value = newInput
-  }
-  
-  const updateOutput = (message) => {
-    output.value += message // 將 WebSocket 回傳的訊息添加到 output
-  }
-  
-  const lockInterpreterType = () => {
-    isInterpreterTypeLocked.value = true
-  }
-  
-  const unlockInterpreterType = () => {
-    isInterpreterTypeLocked.value = false
-  }
-  
-  const clearInputOutput = () => {
-    input.value = ''
-    output.value = ''
-  }
-  
-  const handleConnected = () => {
-    lockInterpreterType()
-    clearInputOutput()
+  } catch (error) {
+    console.error('Error sending POST request:', error)
     $q.notify({
-      type: 'positive',
-      message: '連線成功',
+      type: 'negative',
+      message: '可視化請求失敗',
       timeout: 1200,
       position: 'top',
       progress: true,
     })
   }
-  
-  const handleDisconnected = () => {
-    unlockInterpreterType()
-    $q.notify({
-      type: 'warning',
-      message: '連線中斷',
-      timeout: 1200,
-      position: 'top',
-      progress: true,
-      icon: 'warning'
-    })
-  }
-  
-  const sendPostRequest = async () => {
-    try {
-      const response = await axios.post(`https://visualpl.lab214b.uk:5001/syntax-tree`, {
-        payload: code.value + '\n',
-        interpreterType: `OurScheme${interpreterType.value}`
-      })
-      if (response.status !== 200) {
-        throw new Error('請檢查輸入是否正確')
-      }
-      parseTree.value = response.data.parseTree
-      dialogVisible.value = true
-      gtag('event', 'visualize_syntax_tree', {
-        event_category: 'Syntax Tree',
-        event_label: `OurScheme${interpreterType.value}`,
-      })
-    } catch (error) {
-      console.error('Error sending POST request:', error)
-      $q.notify({
-        type: 'negative',
-        message: '可視化請求失敗',
-        timeout: 1200,
-        position: 'top',
-        progress: true,
-      })
-    }
-  }
-  </script>
+}
+</script>
--- src/pages/TutorialPage.vue
+++ src/pages/TutorialPage.vue
@@ -7,7 +7,9 @@
         <q-step v-for="(stepDetail, index) in steps" :key="index" :name="index + 1" :title="stepDetail.title">
           <div class="step-content">
             <p>{{ stepDetail.description }}</p>
-            <img :src="stepDetail.image" alt="Step Image" class="step-image" v-if="stepDetail.image" />
+            <div v-if="stepDetail.images">
+              <img v-for="(image, imgIndex) in stepDetail.images" :key="imgIndex" :src="image" alt="Step Image" class="step-image" />
+            </div>
             <q-space />
             <div class="button-group">
               <q-btn v-if="index > 0" icon="arrow_back" class="prev-button" color="primary" @click="prevStep" />
@@ -33,27 +35,27 @@ const steps = ref([
   {
     title: '步驟 1: 選擇Project',
     description: '在 OurScheme 中，首先選擇你要測試的Project。',
-    image: 'steps/step1.png'
+    images: ['steps/step1.png']
   },
   {
-    title: '步驟 2: 連線到 Project X',
-    description: '點擊連線按鈕，連線到選擇的 ProjecX。',
-    image: 'steps/step2.png'
+    title: '步驟 2: 連線到 Project',
+    description: '點擊連線按鈕，連線到選擇的 Projec。',
+    images: ['steps/step2.png']
   },
   {
     title: '步驟 3: 輸入程式碼',
     description: '在輸入框中輸入你要執行的程式碼。',
-    image: 'steps/step3.png'
+    images: ['steps/step3.png']
   },
   {
-    title: '步驟 4: 執行程式碼 or 可視化分析',
-    description: '點擊執行按鈕，執行你輸入的程式碼。 或點擊可視化分析按鈕，查看程式碼的語法樹。',
-    image: 'steps/step4.png'
+    title: '步驟 4: 執行程式碼and可視化分析功能選擇',
+    description: '點擊執行按鈕，執行你輸入的程式碼。 若開啟可視化功能按下執行按鈕後會跳出樹狀圖。',
+    images: ['steps/step4-1.png', 'steps/step4-2.png']
   },
   {
     title: '步驟 5: 查看結果',
-    description: '在輸入框與輸出框中檢查程式碼的執行結果。',
-    image: 'steps/step5.png'
+    description: '在輸入框與輸出框中檢查程式碼的執行結果。若開啟可視化功能，會額外跳出樹狀圖。',
+    images: ['steps/step5-1.png', 'steps/step5-2.png']
   }
 ])
 

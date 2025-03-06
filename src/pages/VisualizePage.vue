58a9d0a41bfbc83a49d84b26c8502c97b2bb899d
Joe Liao <joe3727@gmail.com>
2025-03-06 20:31:24 +0800

    更新 VisualizePage，將 iframe 的來源網址更改為新的可視化服務

--- src/pages/VisualizePage.vue
+++ src/pages/VisualizePage.vue
@@ -1,6 +1,6 @@
 <template>
     <q-page>
-      <iframe src="https://memeprod.ap-south-1.linodeobjects.com/user-maker-thumbnail/180a04e4379a720c075a1b3c784c7b9e.gif" style="width: 100%; height: 100vh; border: none;"></iframe>
+      <iframe src="https://visualpl-new.lab214b.uk:5001/" style="width: 100%; height: 100vh; border: none;"></iframe>
     </q-page>
   </template>
   
